0f37b00ab10c034b7bf6ca59a84058fe5f5d1eb2
Linus0006 <llatendr@smail.uni-koeln.de>
2025-11-16 17:04:12 +0100

    OneAndTwo

--- src/h1/H1_main.java
+++ src/h1/H1_main.java
@@ -1,4 +1,28 @@
 package h1;
 
+import java.util.Arrays;
+
 public class H1_main {
+    static void main(String[] args) {
+
+        //variables:
+
+        int[] myArray = {1, 3, 5, 11, 15, 10, 17, 19, 2, 5};
+        int [] myArrayClone1 = myArray.clone();
+        int [] myArrayClone2 = myArray.clone();
+        int laenge = myArray.length;
+
+        //Schleife:
+
+        for (int i = 1; i<= laenge; i++){
+            myArray [laenge - i] = myArrayClone2[i - 1];
+            myArray[i - 1] = myArrayClone1[laenge -i];
+
+
+
+
+
+        }
+        System.out.println(Arrays.toString(myArray));
+    }
 }
--- src/h2/H2_main.java
+++ src/h2/H2_main.java
@@ -1,4 +1,42 @@
 package h2;
 
+import java.util.Arrays;
+
 public class H2_main {
+    static void main(String[] args) {
+
+        //variables able to change:
+
+        int n = 299;
+        int digits = 45;
+
+        //variables not able to change:
+
+        int digitCount = 0;
+        int Zaehler = 1;
+        int [] a = new int[9];
+
+        //Anzahl Ziffern ermitteln:
+
+        while (n>= Zaehler){
+            digitCount++;
+            Zaehler = Zaehler * 10;
+        }
+        if (n == 0){
+            digitCount = 1;
+        }
+
+        digits = digitCount;
+        System.out.println(digits);
+
+        //Belegung in Array:
+
+        String ZahlZuZiffer = String.valueOf(n);
+        for (int i =1 ; i<= digits;i++){
+            a[9-i] = ZahlZuZiffer.charAt(digits-i)-'0';
+        }
+        System.out.println(Arrays.toString(a));
+
+    }
+
 }
